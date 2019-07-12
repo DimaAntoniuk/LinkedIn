2158fdc66bce61aa18b465327d1fb7788c57deb6
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-07-12 17:57:44 +0300

    v1.0 to slow page load to get all links.Algorithm works correct but not all links have been gotten

--- popup.js
+++ popup.js
@@ -1,3 +1,45 @@
-$('.jobs-search-results__list.artdeco-list li').each(function(){
-  console.log($(this).find('.job-card-search__company-name').find('a').attr('href'));
-})
+var links = [];
+
+function scrollDown() {
+  $('.jobs-search-results__list.artdeco-list').animate({ scrollTop: $(document).height() }, 1000);
+}
+
+function checkStartPosotion() {
+  scrollDown();
+  if($('.artdeco-pagination__indicator.artdeco-pagination__indicator--number.active.selected span').text() !== '1') {
+    $('.artdeco-pagination__pages.artdeco-pagination__pages--number li').first().find('button').trigger('click');
+  };
+}
+
+checkStartPosotion();
+
+function parseLinks() {
+  console.log($('.artdeco-pagination__indicator.artdeco-pagination__indicator--number.active.selected span').text());
+  $('.job-card-search__company-name-link').each(function(){
+    if($(this).attr('href') !== '#') {
+      links.push($(this).attr('href'));
+    }
+  });
+  // $('.jobs-search-results__list.artdeco-list li').each(function() {
+  //   links.push($(this).find('.job-card-search__company-name a').attr('href'));
+  // });
+}
+
+function nextPage() {
+  if($('.artdeco-pagination__indicator.artdeco-pagination__indicator--number.active.selected').next().find('button').text()) {
+    $('.artdeco-pagination__indicator.artdeco-pagination__indicator--number.active.selected').next().find('button').trigger('click');
+    parse();
+  } else {
+    parseLinks();
+  };
+}
+
+function parse() {
+  scrollDown();
+  // $(window).load(function() {
+  parseLinks();
+  // });
+  nextPage();
+}
+
+parse();
