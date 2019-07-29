f626ced3ac8d02cc5726d39852ac06b9ca331b24
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-07-29 18:00:54 +0300

    v7.7 fixed last counter bug

--- counter.js
+++ counter.js
@@ -3,6 +3,9 @@ chrome.storage.onChanged.addListener(function() {
     var result_r = result.links.length;
     chrome.storage.local.get('index', function(result) {
       var result_l = result.index + 1;
+      if(result_r < result_l) {
+        result_l = result_r;
+      }
       var text = result_l.toString() + ' / ';
       text += result_r.toString();
       try {
